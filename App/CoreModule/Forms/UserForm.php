6367668ec62c91db9a61635393b945bfef95c631
Josef Kříž <pepakriz@gmail.com>
2012-02-21 14:51:23 +0100

    Fixed UserForm

--- App/CoreModule/Forms/UserForm.php
+++ App/CoreModule/Forms/UserForm.php
@@ -16,22 +16,38 @@ use Nette\Utils\Html;
 /**
  * @author Josef Kříž <pepakriz@gmail.com>
  */
-class UserForm extends \Venne\Forms\EntityForm {
+class UserForm extends \Venne\Forms\EntityForm
+{
 
 	public function startup()
 	{
 		parent::startup();
+
 		$this->addGroup("User");
 		$this->addCheckbox("enable", "Enable")->setDefaultValue(true);
 		$this->addText("email", "E-mail")
 			->addRule(\Nette\Forms\Form::EMAIL, "Enter email");
 		$this->addCheckbox("password_new", "Set password");
-		$this->addPassword("password", "Password")->setOption("description", "minimal length is 5 char")->addConditionOn($this['password_new'], \Nette\Forms\Form::FILLED)->addRule(\Nette\Forms\Form::FILLED, 'Enter password')->addRule(\Nette\Forms\Form::MIN_LENGTH, 'Password is short', 5);
-		$this->addPassword("password_confirm", "Confirm password")->addRule(\Nette\Forms\Form::EQUAL, 'Invalid re password', $this['password']);
+		$this->addPassword("_password", "Password")
+			->setOption("description", "minimal length is 5 char")
+			->addConditionOn($this['password_new'], \Nette\Forms\Form::FILLED)
+			->addRule(\Nette\Forms\Form::FILLED, 'Enter password')
+			->addRule(\Nette\Forms\Form::MIN_LENGTH, 'Password is short', 5);
+		$this->addPassword("password_confirm", "Confirm password")
+			->addRule(\Nette\Forms\Form::EQUAL, 'Invalid re password', $this['_password']);
 		$this->addText("key", "Authentization key")->setOption("description", "If is set user cannot log in.");
 
 		$this->addGroup("Next informations");
 		$this->addManyToMany("roleEntities");
+	}
+
+
+
+	public function handleSuccess()
+	{
+		if ($this["password_new"]->value) {
+			$this->entity->password = $this["_password"]->value;
+		}
 	}
 
 }
