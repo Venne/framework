e3ebc7a71e35d4727f7afe11d956993dfc546ac5
Josef Kříž <pepakriz@gmail.com>
2013-01-03 12:06:49 +0100

    [tests]: Fixed sandbox

--- tests/sandbox.php
+++ tests/sandbox.php
@@ -9,5 +9,6 @@ return array(
 	'tempDir'       => __DIR__ . '/temp',
 	'wwwDir'        => __DIR__ . '/www',
 	'wwwCacheDir'   => __DIR__ . '/www/cache',
+	'publicDir'		=> __DIR__ . '/www/public',
 	'resourcesDir'  => __DIR__ . '/www/resources',
 );
