68a08875e638cf9b5dd6946979c10dd73e41665b
Josef Kříž <pepakriz@gmail.com>
2013-08-28 15:17:20 +0200

    Latte\Marcors\UIMacros: Added new macro - `ifLinkExists`.

--- Venne/Latte/Macros/UIMacros.php
+++ Venne/Latte/Macros/UIMacros.php
@@ -67,6 +67,9 @@ class UIMacros extends \Nette\Latte\Macros\UIMacros
 		$me->addMacro('status', array($me, 'macroStatus'));
 
 		$me->addMacro('path', array($me, 'macroPath'));
+		$me->addMacro('ifLinkExists', array($me, 'macroIfLinkExists'), array($me, 'macroEndIfLinkExists'));
+		$me->addMacro('ifPlinkExists', array($me, 'macroIfLinkExists'), array($me, 'macroEndIfLinkExists'));
+
 		return $me;
 	}
 
@@ -107,5 +110,57 @@ class UIMacros extends \Nette\Latte\Macros\UIMacros
 	{
 		return $writer->write("echo \$basePath . '/' . \$presenter->context->venne->moduleHelpers->expandResource(%node.word)");
 	}
-}
 
+
+	/**
+	 * {ifLinkExists destination [,] [params]}
+	 * n:ifLinkExists="destination [,] [params]"
+	 */
+	public function macroIfLinkExists(MacroNode $node, PhpWriter $writer)
+	{
+		$c = '$_macroIfLinkExistsOld = $_presenter->invalidLinkMode;
+		$_presenter->invalidLinkMode = $_presenter::INVALID_LINK_EXCEPTION;
+		try {
+			$_macroIfLinkExistslink = %escape(%modify(' . ($node->name === 'ifPlinkExists' ? '$_presenter' : '$_control') . '->link(%node.word, %node.array?)));
+			$_macroIfLinkExistsRet = TRUE;
+		} catch (\Nette\Application\UI\InvalidLinkException $e) {
+			$_macroIfLinkExistslink = \'#\';
+			$_macroIfLinkExistsRet = FALSE;
+			$_presenter->invalidLinkMode = $_macroIfLinkExistsOld;
+		}
+		$_presenter->invalidLinkMode = $_macroIfLinkExistsOld;
+		';
+		if ($node->prefix === $node::PREFIX_TAG) {
+			return $writer->write($c . ($node->htmlNode->closing ? 'if (array_pop($_l->ifs)):' : 'if ($_l->ifs[] = ($_macroIfLinkExistsRet)):'));
+		}
+		return $writer->write($c . 'if ($_macroIfLinkExistsRet):');
+	}
+
+
+	/**
+	 * {/ifLinkExists}
+	 */
+	public function macroEndIfLinkExists(MacroNode $node, PhpWriter $writer)
+	{
+		return 'endif';
+	}
+
+
+	/**
+	 * {link destination [,] [params]}
+	 * {plink destination [,] [params]}
+	 * n:href="destination [,] [params]"
+	 * n:link
+	 */
+	public function macroLink(MacroNode $node, PhpWriter $writer)
+	{
+		if ($node->args == '') {
+			if ($node->htmlNode) {
+				$node->attrCode = $writer->write(' href="<?php echo $_macroIfLinkExistslink; ?>"');
+				return;
+			}
+			return $writer->write('echo $_macroIfLinkExistslink');
+		}
+		return parent::macroLink($node, $writer);
+	}
+}
